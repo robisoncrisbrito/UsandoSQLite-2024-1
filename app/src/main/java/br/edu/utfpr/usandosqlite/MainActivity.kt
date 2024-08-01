b99fa655e6b66293ff7681c25d79c8de14bd45e9
robisoncrisbrito <robison.brito@gmail.com>
2024-08-01 20:39:29 -0300

    .

--- app/src/main/java/br/edu/utfpr/usandosqlite/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite/MainActivity.kt
@@ -1,14 +1,36 @@
 package br.edu.utfpr.usandosqlite
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCod : EditText
+    private lateinit var etNome : EditText
+    private lateinit var etTelefone : EditText
+
+    private lateinit var banco : SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etCod = findViewById( R.id.etCod )
+        etNome = findViewById( R.id.etNome )
+        etTelefone = findViewById( R.id.etTelefone )
+
+        banco = SQLiteDatabase.openOrCreateDatabase( this.getDatabasePath( "dbfile.sqlite" ), null )
     }
+
+    fun btIncluirOnClick(view: View) {}
+    fun btAlterarOnClick(view: View) {}
+    fun btExcluirOnClick(view: View) {}
+    fun btPesquisarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
 }
