cfb7cfb0d0b151017452ee795dd37f687be6dac8
robisoncrisbrito <robison.brito@gmail.com>
2024-08-15 20:42:40 -0300

    .

---
+++ app/src/main/java/br/edu/utfpr/usandosqlite/adapter/ElementoListaAdapter.kt
@@ -0,0 +1,26 @@
+package br.edu.utfpr.usandosqlite.adapter
+
+import android.content.Context
+import android.database.Cursor
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ElementoListaAdapter (val context : Context, val cursor : Cursor) : BaseAdapter() {
+
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Any {
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    }
+}
